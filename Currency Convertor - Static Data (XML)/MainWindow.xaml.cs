c22e056c230344c1d2fb1641666aa4075b8b45b2
Alireza Mak <info@alirezamak.com>
2025-08-09 19:01:13 -0700

    Add currency converter UI layout with Font Awesome icon, custom rounded button styles, and click event handlers in code-behind

--- Currency Convertor - Static Data (XML)/MainWindow.xaml.cs
+++ Currency Convertor - Static Data (XML)/MainWindow.xaml.cs
@@ -11,5 +11,14 @@ namespace Currency_Convertor___Static_Data__XML_
         {
             InitializeComponent();
         }
+        public void Clear_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        public void Convert_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        public void NumberValidationTextBox(object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
